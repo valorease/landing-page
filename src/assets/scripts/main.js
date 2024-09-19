9cfc5a480910c662261274b6c89aca3cf1cb7e4e
Pedro Veloso <86307207+pedrohaveloso@users.noreply.github.com>
2024-09-19 20:52:54 -0300

    chore: Add .gitignore, landing page CSS, and postcss.config.js
    - Added .gitignore file to exclude unnecessary files and directories from version control.
    - Added landing page CSS file to style the 404 page.
    - Added postcss.config.js file to configure PostCSS plugins for CSS processing.

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+export class FlexIcon extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  static cache = new Map();
+
+  async connectedCallback() {
+    const name = this.getAttribute("name");
+
+    if (!name) {
+      return;
+    }
+
+    if (!FlexIcon.cache.has(name)) {
+      const response = await fetch(`./assets/icons/${name}.svg`).catch(
+        () => ""
+      );
+
+      FlexIcon.cache.set(
+        name,
+        response instanceof Response && response.status === 200
+          ? await response.text()
+          : ""
+      );
+    }
+
+    this.innerHTML = FlexIcon.cache.get(name);
+  }
+}
+
+customElements.define("f-icon", FlexIcon);
